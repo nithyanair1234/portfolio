3ded4aaaf0c50383594ef752fb9141f968f0ad03
nithyanair1234 <nithya.nair10@gmail.com>
2026-04-16 13:07:23 -0700

    theme switch

--- global.js
+++ global.js
@@ -13,37 +13,68 @@ let pages = [
     { url: 'https://github.com/nithyanair1234', title: 'Profile' },
   ];
   
-  // 2. Detect environment (Local vs GitHub Pages)
-  // Replace "portfolio" with your actual repo name!
   const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/" 
-    : "/portfolio/"; // Must match your GitHub repo name exactly!
+    : "/portfolio/"; 
   
-  // 3. Create the Nav container
+  
   let nav = document.createElement('nav');
   document.body.prepend(nav);
   
-  // 4. Loop and build the links
+  
   for (let p of pages) {
     let url = p.url;
     let title = p.title;
   
-    // Adjust URL for local vs live
+    
     url = !url.startsWith('http') ? BASE_PATH + url : url;
   
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
   
-    // Highlight current page
+    
     if (a.host === location.host && a.pathname === location.pathname) {
         a.classList.add('current');
     }
   
-    // Open external links in new tab
+   
     if (a.host !== location.host) {
       a.target = "_blank";
     }
   
     nav.append(a);
   }
+
+  document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+    `
+  );
+
+  let select = document.querySelector('.color-scheme select');
+
+  if ("colorScheme" in localStorage) {
+    let savedScheme = localStorage.colorScheme;
+
+    document.documentElement.style.setProperty('color-scheme', savedScheme);
+
+    select.value = savedScheme;
+  }
+
+  select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+
+    localStorage.colorScheme = event.target.value;
+  });
+
